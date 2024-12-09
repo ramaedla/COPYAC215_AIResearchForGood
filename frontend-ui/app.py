d9ea4861d09daefc8709a83bd8a5c5c22870b28f
ramaedla <ramasandeepedlabadkar@fas.harvard.edu>
2024-12-08 22:15:43 -0500

    Update app.py

--- frontend-ui/app.py
+++ frontend-ui/app.py
@@ -1,13 +1,26 @@
 import streamlit as st
 import sys
-import os
+import os, json
 
 # Add the src directory to the path to import perform_rag
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src', 'perform_rag')))
 from perform_rag import main as perform_rag_main
 
 print("helo")
-st.write(st.secrets['private_key_id'])
+st.write(st.secrets)
+
+st.write(st.secrets)
+
+secrets_dict = dict(st.secrets)
+
+# Fix formatting for private_key if necessary
+if "private_key" in secrets_dict:
+    secrets_dict["private_key"] = secrets_dict["private_key"].replace("\\n", "\n")
+
+# Convert the dictionary to JSON
+json_data = json.dumps(secrets_dict, indent=4)
+
+st.write(json_data)
 
 # Streamlit UI
 st.title("Global Tech Colab For Good: A Platform for Non-Profits and Research Groups")
